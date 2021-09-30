0c6b6b76d037d6e5f7a1d26a757d7d67347349c4
bitbucket-pipeline <commits-noreply@bitbucket.com>
2021-09-30 16:44:04 +0000

    Build Number 576

--- build/assets/js/scripts.js
+++ build/assets/js/scripts.js
@@ -4666,6 +4666,19 @@ const FORM_VISITAS = document.getElementById('form-visitas');
 const INFO_ADMINISTRACION = document.getElementById('info-administracion');
 const INFO_UBICACION = document.getElementById('info-ubicacion');
 
+const BOX_CONTACTO_MOBILE = document.getElementById("box-contacto-mobile");
+const BOX_VISITAS_MOBILE = document.getElementById("box-visitas-mobile");
+const BOX_ADMINISTRACION_MOBILE = document.getElementById("box-administracion-mobile");
+const BOX_UBICACION_MOBILE = document.getElementById("box-ubicacion-mobile");
+
+const FORM_CONTACTO_MOBILE = document.getElementById('form-contacto-mobile');
+const FORM_VISITAS_MOBILE = document.getElementById('form-visitas-mobile');
+const INFO_ADMINISTRACION_MOBILE = document.getElementById('info-administracion-mobile');
+const INFO_UBICACION_MOBILE = document.getElementById('info-ubicacion-mobile');
+
+
+
+// =================DESKTOP FUNCTIONS=================
 function resetForms() {
     BOX_CONTACTO.classList.remove('contact-section-inactive');
     BOX_CONTACTO.classList.add('contact-section-selected');
@@ -4730,4 +4743,73 @@ function showUbicacion(){
     FORM_VISITAS.classList.add('is-hidden');
     INFO_ADMINISTRACION.classList.add('is-hidden');
     INFO_UBICACION.classList.remove('is-hidden');
+}
+
+
+// =================MOBILE FUNCTIONS=================
+
+function resetFormsMobile() {
+    BOX_CONTACTO_MOBILE.classList.remove('contact-section-inactive');
+    BOX_CONTACTO_MOBILE.classList.add('contact-section-selected');
+
+    BOX_VISITAS_MOBILE.classList.add('contact-section-inactive');
+    BOX_VISITAS_MOBILE.classList.remove('contact-section-selected');
+    BOX_ADMINISTRACION_MOBILE.classList.add('contact-section-inactive');
+    BOX_ADMINISTRACION_MOBILE.classList.remove('contact-section-selected');
+    BOX_UBICACION_MOBILE.classList.add('contact-section-inactive');
+    BOX_UBICACION_MOBILE.classList.remove('contact-section-selected');
+    
+    FORM_CONTACTO_MOBILE.classList.remove('is-hidden');
+    FORM_VISITAS_MOBILE.classList.add('is-hidden');
+    INFO_ADMINISTRACION_MOBILE.classList.add('is-hidden');
+    INFO_UBICACION_MOBILE.classList.add('is-hidden');
+}
+
+function showVisitasMobile(){
+    BOX_VISITAS_MOBILE.classList.remove('contact-section-inactive');
+    BOX_VISITAS_MOBILE.classList.add('contact-section-selected');
+
+    BOX_CONTACTO_MOBILE.classList.add('contact-section-inactive');
+    BOX_CONTACTO_MOBILE.classList.remove('contact-section-selected');
+    BOX_ADMINISTRACION_MOBILE.classList.add('contact-section-inactive');
+    BOX_ADMINISTRACION_MOBILE.classList.remove('contact-section-selected');
+    BOX_UBICACION_MOBILE.classList.add('contact-section-inactive');
+    BOX_UBICACION_MOBILE.classList.remove('contact-section-selected');
+
+    FORM_CONTACTO_MOBILE.classList.add('is-hidden');
+    FORM_VISITAS_MOBILE.classList.remove('is-hidden');
+    INFO_ADMINISTRACION_MOBILE.classList.add('is-hidden');
+    INFO_UBICACION_MOBILE.classList.add('is-hidden');
+}
+function showAdministracionMobile(){
+    BOX_ADMINISTRACION_MOBILE.classList.remove('contact-section-inactive');
+    BOX_ADMINISTRACION_MOBILE.classList.add('contact-section-selected');
+
+    BOX_VISITAS_MOBILE.classList.add('contact-section-inactive');
+    BOX_VISITAS_MOBILE.classList.remove('contact-section-selected');
+    BOX_CONTACTO_MOBILE.classList.add('contact-section-inactive');
+    BOX_CONTACTO_MOBILE.classList.remove('contact-section-selected');
+    BOX_UBICACION_MOBILE.classList.add('contact-section-inactive');
+    BOX_UBICACION_MOBILE.classList.remove('contact-section-selected');
+
+    FORM_CONTACTO_MOBILE.classList.add('is-hidden');
+    FORM_VISITAS_MOBILE.classList.add('is-hidden');
+    INFO_ADMINISTRACION_MOBILE.classList.remove('is-hidden');
+    INFO_UBICACION_MOBILE.classList.add('is-hidden');
+}
+function showUbicacionMobile(){
+    BOX_UBICACION_MOBILE.classList.remove('contact-section-inactive');
+    BOX_UBICACION_MOBILE.classList.add('contact-section-selected');
+
+    BOX_ADMINISTRACION_MOBILE.classList.add('contact-section-inactive');
+    BOX_ADMINISTRACION_MOBILE.classList.remove('contact-section-selected');
+    BOX_VISITAS_MOBILE.classList.add('contact-section-inactive');
+    BOX_VISITAS_MOBILE.classList.remove('contact-section-selected');
+    BOX_CONTACTO_MOBILE.classList.add('contact-section-inactive');
+    BOX_CONTACTO_MOBILE.classList.remove('contact-section-selected');
+
+    FORM_CONTACTO_MOBILE.classList.add('is-hidden');
+    FORM_VISITAS_MOBILE.classList.add('is-hidden');
+    INFO_ADMINISTRACION_MOBILE.classList.add('is-hidden');
+    INFO_UBICACION_MOBILE.classList.remove('is-hidden');
 }
