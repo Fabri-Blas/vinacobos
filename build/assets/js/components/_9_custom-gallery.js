bd92c64251fa95385752a3158567ca40ffa09673
bitbucket-pipeline <commits-noreply@bitbucket.com>
2021-10-25 19:35:04 +0000

    Build Number 783

--- build/assets/js/components/_9_custom-gallery.js
+++ build/assets/js/components/_9_custom-gallery.js
@@ -9,168 +9,180 @@ class galleryPreview {
 
 
 // =================================GALLERY VARS=================================
-let galleryActual = document.querySelector('.gallery-actual');
-let galleryActualMobile = document.querySelector('.gallery-actual-mobile');
+const mainGalleryVars = document.querySelectorAll('.main-gallery');
+const mainGalleryVarsMobile = document.querySelectorAll('.main-gallery-mobile');
 
 // =================================DESKTOP GALLERY=================================
-if (galleryActual) {
+if (mainGalleryVars) {
+    mainGalleryVars.forEach(mainGallery => {
+        let galleryActual = mainGallery.querySelector('.gallery-actual');
 
-    galleryActual = galleryActual.firstElementChild;
-    let galleryPreviewBackdrop = document.querySelectorAll('.gallery-preview-selected');
+        if (galleryActual) {
 
-    clearGalleryBackdrops();
+            galleryActual = galleryActual.firstElementChild;
+            let galleryPreviewBackdrop = mainGallery.querySelectorAll('.gallery-preview-selected');
 
-    galleryPreviewBackdrop.forEach(previewBackdrop => {
-        previewBackdrop.addEventListener('click', () => {
             clearGalleryBackdrops();
-            setGalleryBackdrops(previewBackdrop);
-        });
-    });
 
-    const allGalleryPreviews = document.querySelectorAll('.gallery-preview');
-    const previewsCont = allGalleryPreviews.length / 2;
+            galleryPreviewBackdrop.forEach(previewBackdrop => {
+                previewBackdrop.addEventListener('click', () => {
+                    clearGalleryBackdrops();
+                    setGalleryBackdrops(previewBackdrop);
+                });
+            });
 
-    const galleryObjects = [];
+            const allGalleryPreviews = mainGallery.querySelectorAll('.gallery-preview');
+            const previewsCont = allGalleryPreviews.length;
 
-    for (let i = 0; i < previewsCont; i++) {
-        const galleryObject = new galleryPreview(
-            preview = allGalleryPreviews[i],
-            path = allGalleryPreviews[i].childNodes[3].getAttribute('gallery-src'),
-            false
-        );
-        galleryObjects.push(galleryObject);
-    }
+            const galleryObjects = [];
 
-    galleryObjects.forEach(gallery => {
-        galleryClickEvent(gallery);
-    });
-
-    clearGalleryBackdrops();
-    clearGallerySelect();
-    galleryObjects[0].preview.click();
-    setGalleryBackdrops(galleryPreviewBackdrop[0]);
-
-    function clearGalleryBackdrops() {
-        galleryPreviewBackdrop.forEach(preview => {
-            preview.style.opacity = '0';
-            preview.firstElementChild.setAttribute('style', 'transform: scale(0.5) rotate(-360deg); opacity: 0;');
-        });
-    }
-
-    function setGalleryBackdrops(preview) {
-        preview.style.opacity = '1';
-        preview.firstElementChild.setAttribute('style', 'transform: scale(1) rotate(0deg); opacity: 1;');
-    }
-
-    function galleryClickEvent(gallery) {
-        gallery.preview.addEventListener('click', () => {
-            if (!gallery.selected) {
-                clearGallerySelect(); gallerySelect(gallery);
-                gallery.selected = true;
+            for (let i = 0; i < previewsCont; i++) {
+                const galleryObject = new galleryPreview(
+                    preview = allGalleryPreviews[i],
+                    path = allGalleryPreviews[i].childNodes[3].getAttribute('gallery-src'),
+                    false
+                );
+                galleryObjects.push(galleryObject);
             }
-        });
-    }
 
-    function clearGallerySelect() {
-        galleryObjects.forEach(gallery => {
-            gallery.preview.firstElementChild.style.boxShadow = "none";
-            gallery.selected = false;
-        });
-    }
+            galleryObjects.forEach(gallery => {
+                galleryClickEvent(gallery);
+            });
 
-    function gallerySelect(gallery) {
-        galleryActual.style.opacity = '0';
-        galleryActual.style.filter = 'blur(20px)';
-        galleryActual.style.transform = 'translateY(30px)';
+            clearGalleryBackdrops();
+            clearGallerySelect();
+            galleryObjects[0].preview.click();
+            setGalleryBackdrops(galleryPreviewBackdrop[0]);
 
-        setTimeout(() => {
-            galleryActual.setAttribute('src', gallery.path);
-            galleryActual.style.opacity = '1';
-            galleryActual.style.filter = 'blur(0px)';
-            galleryActual.style.transform = 'translateY(0px)';
-        }, 900);
-    }
+            function clearGalleryBackdrops() {
+                galleryPreviewBackdrop.forEach(preview => {
+                    preview.style.opacity = '0';
+                    preview.firstElementChild.setAttribute('style', 'transform: scale(0.5) rotate(-360deg); opacity: 0;');
+                });
+            }
 
+            function setGalleryBackdrops(preview) {
+                preview.style.opacity = '1';
+                preview.firstElementChild.setAttribute('style', 'transform: scale(1) rotate(0deg); opacity: 1;');
+            }
+
+            function galleryClickEvent(gallery) {
+                gallery.preview.addEventListener('click', () => {
+                    if (!gallery.selected) {
+                        clearGallerySelect(); gallerySelect(gallery);
+                        gallery.selected = true;
+                    }
+                });
+            }
+
+            function clearGallerySelect() {
+                galleryObjects.forEach(gallery => {
+                    gallery.preview.firstElementChild.style.boxShadow = "none";
+                    gallery.selected = false;
+                });
+            }
+
+            function gallerySelect(gallery) {
+                galleryActual.style.opacity = '0';
+                galleryActual.style.filter = 'blur(20px)';
+                galleryActual.style.transform = 'translateY(30px)';
+
+                setTimeout(() => {
+                    galleryActual.setAttribute('src', gallery.path);
+                    galleryActual.style.opacity = '1';
+                    galleryActual.style.filter = 'blur(0px)';
+                    galleryActual.style.transform = 'translateY(0px)';
+                }, 900);
+            }
+
+        }
+    });
 }
 
 // =================================MOBILE GALLERY=================================
-if (galleryActualMobile) {
+if (mainGalleryVarsMobile) {
+    mainGalleryVarsMobile.forEach(mainGalleryMobile => {
+        let galleryActualMobile = mainGalleryMobile.querySelector('.gallery-actual-mobile');
 
-    galleryActualMobile = galleryActualMobile.firstElementChild;
+        if (galleryActualMobile) {
 
-    let galleryPreviewBackdropMobile = document.querySelectorAll('.gallery-preview-selected-mobile');
+            galleryActualMobile = galleryActualMobile.firstElementChild;
 
-    clearGalleryBackdropsMobile();
+            let galleryPreviewBackdropMobile = mainGalleryMobile.querySelectorAll('.gallery-preview-selected-mobile');
 
-    galleryPreviewBackdropMobile.forEach(preview => {
-        preview.addEventListener('click', () => {
             clearGalleryBackdropsMobile();
-            setGalleryBackdropsMobile(preview);
-        });
-    });
+
+            galleryPreviewBackdropMobile.forEach(preview => {
+                preview.addEventListener('click', () => {
+                    clearGalleryBackdropsMobile();
+                    setGalleryBackdropsMobile(preview);
+                });
+            });
 
 
-    const galleryObjectsMobile = [];
+            const galleryObjectsMobile = [];
 
-    const allGalleryPreviewsMobile = document.querySelectorAll('.gallery-preview');
-    const previewsContMobile = allGalleryPreviewsMobile.length / 2;
+            const allGalleryPreviewsMobile = mainGalleryMobile.querySelectorAll('.gallery-preview');
+            const previewsContMobile = allGalleryPreviewsMobile.length;
 
-    for (let i = previewsContMobile * 2 - 1; i >= previewsContMobile; i--) {
-        const galleryObject = new galleryPreview(
-            preview = allGalleryPreviewsMobile[i],
-            path = allGalleryPreviewsMobile[i].childNodes[3].getAttribute('gallery-src'),
-            false
-        )
-        galleryObjectsMobile.unshift(galleryObject);
-    }
-
-
-    galleryObjectsMobile.forEach(gallery => {
-        galleryClickEventMobile(gallery);
-    });
-
-    clearGalleryBackdropsMobile();
-    clearGallerySelectMobile();
-    galleryObjectsMobile[0].preview.click();
-    setGalleryBackdropsMobile(galleryPreviewBackdropMobile[0]);
-
-    function galleryClickEventMobile(gallery) {
-        gallery.preview.addEventListener('click', () => {
-            if (!gallery.selected) {
-                clearGallerySelectMobile(); gallerySelectMobile(gallery);
-                gallery.selected = true;
+            for (let i = 0; i < previewsContMobile; i++) {
+                const galleryObject = new galleryPreview(
+                    preview = allGalleryPreviewsMobile[i],
+                    path = allGalleryPreviewsMobile[i].childNodes[3].getAttribute('gallery-src'),
+                    false
+                )
+                galleryObjectsMobile.push(galleryObject);
             }
-        });
-    }
 
-    function clearGallerySelectMobile() {
-        galleryObjectsMobile.forEach(gallery => {
-            gallery.selected = false;
-        });
-    }
 
-    function clearGalleryBackdropsMobile() {
-        galleryPreviewBackdropMobile.forEach(preview => {
-            preview.style.opacity = '0';
-            preview.firstElementChild.setAttribute('style', 'transform: scale(0.5) rotate(-360deg); opacity: 0;');
-        });
-    }
-    function setGalleryBackdropsMobile(preview) {
-        preview.style.opacity = '1';
-        preview.firstElementChild.setAttribute('style', 'transform: scale(1) rotate(0deg); opacity: 1;');
-    }
+            galleryObjectsMobile.forEach(gallery => {
+                galleryClickEventMobile(gallery);
+            });
 
-    function gallerySelectMobile(gallery) {
-        galleryActualMobile.style.opacity = '0';
-        galleryActualMobile.style.filter = 'blur(20px)';
-        galleryActualMobile.style.transform = 'translateY(30px)';
+            clearGalleryBackdropsMobile();
+            clearGallerySelectMobile();
+            galleryObjectsMobile[0].preview.click();
+            setGalleryBackdropsMobile(galleryPreviewBackdropMobile[0]);
 
-        setTimeout(() => {
-            galleryActualMobile.setAttribute('src', gallery.path);
-            galleryActualMobile.style.opacity = '1';
-            galleryActualMobile.style.filter = 'blur(0px)';
-            galleryActualMobile.style.transform = 'translateY(0px)';
-        }, 900);
-    }
+            function galleryClickEventMobile(gallery) {
+                gallery.preview.addEventListener('click', () => {
+                    if (!gallery.selected) {
+                        clearGallerySelectMobile(); gallerySelectMobile(gallery);
+                        gallery.selected = true;
+                    }
+                });
+            }
+
+            function clearGallerySelectMobile() {
+                galleryObjectsMobile.forEach(gallery => {
+                    gallery.selected = false;
+                });
+            }
+
+            function clearGalleryBackdropsMobile() {
+                galleryPreviewBackdropMobile.forEach(preview => {
+                    preview.style.opacity = '0';
+                    preview.firstElementChild.setAttribute('style', 'transform: scale(0.5) rotate(-360deg); opacity: 0;');
+                });
+            }
+            function setGalleryBackdropsMobile(preview) {
+                preview.style.opacity = '1';
+                preview.firstElementChild.setAttribute('style', 'transform: scale(1) rotate(0deg); opacity: 1;');
+            }
+
+            function gallerySelectMobile(gallery) {
+                galleryActualMobile.style.opacity = '0';
+                galleryActualMobile.style.filter = 'blur(20px)';
+                galleryActualMobile.style.transform = 'translateY(30px)';
+
+                setTimeout(() => {
+                    galleryActualMobile.setAttribute('src', gallery.path);
+                    galleryActualMobile.style.opacity = '1';
+                    galleryActualMobile.style.filter = 'blur(0px)';
+                    galleryActualMobile.style.transform = 'translateY(0px)';
+                }, 900);
+            }
+        }
+
+    });
 }
-
