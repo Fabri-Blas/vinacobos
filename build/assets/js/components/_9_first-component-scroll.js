e10c42206200593d18c97fe2b4d3840614452284
bitbucket-pipeline <commits-noreply@bitbucket.com>
2021-11-23 16:08:49 +0000

    Build Number 962

--- build/assets/js/components/_9_first-component-scroll.js
+++ build/assets/js/components/_9_first-component-scroll.js
@@ -9,34 +9,39 @@ if (firstComponentAnimation && secondComponentReveal && window.innerWidth > 1024
     window.scrollTo({ top: 0, behavior: 'smooth' });
 
     window.addEventListener('scroll', () => {
-        revealFirstComponent();
+        revealFirstComponent(e);
+        resetFirstComponent(e);
     });
 
     window.addEventListener('wheel', (e) => {
-        revealFirstComponent();
+        revealFirstComponent(e);
+        resetFirstComponent(e);
+    });
 
-        if (window.scrollY == 0 && !firstComponentHidden && getDelta() < 0) {
+    function revealFirstComponent(e) {
+        if (window.scrollY == 0 && firstComponentHidden && getDelta(e) > 0) {
+            setTimeout(() => {
+                firstComponentHidden = false;
+            }, 1000);
+            document.body.style.overflowY = 'hidden';
+            firstComponentAnimation.style.top = "50%";
+            secondComponentReveal.style.backgroundColor = 'black';
+            secondComponentReveal.style.backdropFilter = 'blur(15px)';
+        }
+    }
+    function resetFirstComponent(e) {
+        if (window.scrollY == 0 && !firstComponentHidden && getDelta(e) < 0) {
             setTimeout(() => {
                 document.body.style.overflowY = 'overlay';
                 firstComponentHidden = true;
-            }, 800);
+            }, 1000);
             firstComponentAnimation.style.top = '-52%';
             secondComponentReveal.style.backgroundColor = 'transparent';
             secondComponentReveal.style.backdropFilter = 'blur(0px)';
         }
-    });
-
-    function revealFirstComponent() {
-        if (window.scrollY == 0 && getDelta() > 0) {
-            firstComponentAnimation.style.top = "50%";
-            secondComponentReveal.style.backgroundColor = 'black';
-            secondComponentReveal.style.backdropFilter = 'blur(15px)';
-            document.body.style.overflowY = 'hidden';
-            firstComponentHidden = false;
-        }
     }
 
-    function getDelta() {
+    function getDelta(e) {
         var e = window.event || e;
         return Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
     }
