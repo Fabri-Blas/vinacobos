2b3135ec8bbe62dbe9c9b75678dc2e8d08f679b3
bitbucket-pipeline <commits-noreply@bitbucket.com>
2021-11-23 19:25:10 +0000

    Build Number 968

--- build/assets/js/scripts.js
+++ build/assets/js/scripts.js
@@ -4531,7 +4531,7 @@ const cinemagraphs = document.querySelectorAll('.cinemagraph-speed');
 if(cinemagraphs.length > 0){
     cinemagraphs.forEach(graph => {
         graph.playbackRate = 0.75;
-        console.log(graph.playbackRate);
+        console.log(`Cinemagraph speed: ${graph.playbackRate * 100}%`);
     });
 }
 const terroirContenedor = document.getElementById('origen-contenedor');
