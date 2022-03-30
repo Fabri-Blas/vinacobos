f5f31749a6940cd4d62070875a2cbd28d9bab529
bitbucket-pipeline <commits-noreply@bitbucket.com>
2022-03-30 18:42:28 +0000

    Build Number 131

--- build/assets/js/scripts.js
+++ build/assets/js/scripts.js
@@ -2268,6 +2268,45 @@ function maps() {
     hrBtnMaps.classList.remove('hide')
 }
 
+const buttonTeamwork = document.getElementById('teamwork')
+const buttonRespect = document.getElementById('respect')
+const buttonAtention = document.getElementById('atention')
+
+const carouselTeamwork = document.getElementById('carousel-teamwork')
+const carouselRespect = document.getElementById('carousel-respect')
+const carouselAtention = document.getElementById('carousel-atention')
+
+
+function teamwork() {
+    buttonTeamwork.classList.add('identity-active')
+    buttonAtention.classList.remove('identity-active')
+    buttonRespect.classList.remove('identity-active')
+
+    carouselAtention.classList.add('hide')
+    carouselRespect.classList.add('hide')
+    carouselTeamwork.classList.remove('hide')
+}
+
+function respect() {
+    buttonTeamwork.classList.remove('identity-active')
+    buttonAtention.classList.remove('identity-active')
+    buttonRespect.classList.add('identity-active')
+
+    carouselAtention.classList.add('hide')
+    carouselRespect.classList.remove('hide')
+    carouselTeamwork.classList.add('hide')
+}
+
+function atention() {
+    buttonTeamwork.classList.remove('identity-active')
+    buttonAtention.classList.add('identity-active')
+    buttonRespect.classList.remove('identity-active')
+
+    carouselAtention.classList.remove('hide')
+    carouselRespect.classList.add('hide')
+    carouselTeamwork.classList.add('hide')
+}
+
 const sectionWines = document.getElementById('wines-list')
 const infoCobos = document.getElementById('info-cobos')
 const infoVolturno = document.getElementById('info-volturno')
