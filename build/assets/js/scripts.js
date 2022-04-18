4e2e666f8da2f4cab4a7d20caf25142cda20566b
bitbucket-pipeline <commits-noreply@bitbucket.com>
2022-04-18 13:32:44 +0000

    Build Number 215

--- build/assets/js/scripts.js
+++ build/assets/js/scripts.js
@@ -2431,10 +2431,8 @@ function headerLinksAnimation(links, opacity, transform, timing) {
         }, t += timing)
     })
 }
-const es = document.getElementById('es');
-const en = document.getElementById('en');
 const home = document.getElementById('home');
-const history = document.getElementById('history');
+const historia = document.getElementById('history');
 const identity = document.getElementById('identity');
 const farming = document.getElementById('farming');
 const wines = document.getElementById('wines')
@@ -2444,7 +2442,92 @@ const legal = document.getElementById('legal');
 
 
 
+if (window.location.pathname === '/index.html') {
+    home.classList.add('color-subtitle-active');
+    historia.classList.remove('color-subtitle-active');
+    identity.classList.remove('color-subtitle-active');
+    farming.classList.remove('color-subtitle-active');
+    wines.classList.remove('color-subtitle-active');
+    hospitality.classList.remove('color-subtitle-active');
+    shop.classList.remove('color-subtitle-active')
+    legal.classList.remove('color-subtitle-active')
+     
+}  
 
+if (window.location.pathname === '/history.html') {
+    historia.classList.add('color-subtitle-active');
+    home.classList.remove('color-subtitle-active');
+    identity.classList.remove('color-subtitle-active');
+    farming.classList.remove('color-subtitle-active');
+    wines.classList.remove('color-subtitle-active');
+    hospitality.classList.remove('color-subtitle-active');
+    shop.classList.remove('color-subtitle-active')
+    legal.classList.remove('color-subtitle-active')
+     
+}  
+
+if (window.location.pathname === '/identity.html') {
+    identity.classList.add('color-subtitle-active');
+    historia.classList.remove('color-subtitle-active');
+    home.classList.remove('color-subtitle-active');
+    farming.classList.remove('color-subtitle-active');
+    wines.classList.remove('color-subtitle-active');
+    hospitality.classList.remove('color-subtitle-active');
+    shop.classList.remove('color-subtitle-active')
+    legal.classList.remove('color-subtitle-active')
+     
+}  
+
+
+if (window.location.pathname === '/farming.html') {
+    farming.classList.add('color-subtitle-active');
+    historia.classList.remove('color-subtitle-active');
+    identity.classList.remove('color-subtitle-active');
+    home.classList.remove('color-subtitle-active');
+    wines.classList.remove('color-subtitle-active');
+    hospitality.classList.remove('color-subtitle-active');
+    shop.classList.remove('color-subtitle-active')
+    legal.classList.remove('color-subtitle-active')
+     
+}  
+
+
+if (window.location.pathname === '/wines.html' || window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/')) === '/wines') {
+    wines.classList.add('color-subtitle-active');
+    historia.classList.remove('color-subtitle-active');
+    identity.classList.remove('color-subtitle-active');
+    farming.classList.remove('color-subtitle-active');
+    home.classList.remove('color-subtitle-active');
+    hospitality.classList.remove('color-subtitle-active');
+    shop.classList.remove('color-subtitle-active')
+    legal.classList.remove('color-subtitle-active')
+     
+}  
+
+
+if (window.location.pathname === '/hospitality.html') {
+    hospitality.classList.add('color-subtitle-active');
+    historia.classList.remove('color-subtitle-active');
+    identity.classList.remove('color-subtitle-active');
+    farming.classList.remove('color-subtitle-active');
+    wines.classList.remove('color-subtitle-active');
+    home.classList.remove('color-subtitle-active');
+    shop.classList.remove('color-subtitle-active')
+    legal.classList.remove('color-subtitle-active')
+     
+}  
+
+if (window.location.pathname === '/legal.html') {
+    legal.classList.add('color-subtitle-active');
+    historia.classList.remove('color-subtitle-active');
+    identity.classList.remove('color-subtitle-active');
+    farming.classList.remove('color-subtitle-active');
+    wines.classList.remove('color-subtitle-active');
+    hospitality.classList.remove('color-subtitle-active');
+    shop.classList.remove('color-subtitle-active')
+    home.classList.remove('color-subtitle-active')
+     
+}  
 const gif = document.getElementById('gif-history')
 const img = document.getElementById('img-history')
 
