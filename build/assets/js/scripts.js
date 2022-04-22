589aeadbb734fa3b52819008aaa6d96f35c470c7
bitbucket-pipeline <commits-noreply@bitbucket.com>
2022-04-22 22:03:09 +0000

    Build Number 270

--- build/assets/js/scripts.js
+++ build/assets/js/scripts.js
@@ -2720,29 +2720,29 @@ if (window.location.pathname === '/legal.html') {
 
 
 function selectedPage() {
-    if (window.location.pathname.substring(1, window.location.pathname.lastIndexOf('.html')) === 'index') {
+    if (window.location.pathname.substring(1, window.location.pathname.lastIndexOf('.html')) === 'index' || window.location.pathname === '/') {
         document.getElementById('pageSelected').innerHTML +=
-            '<div class="bg-black color-white text-vertical width-100% text-uppercase">home</div>'
+            '<div class="bg-black color-white text-vertical width-100% text-uppercase font-secondary">home</div>'
     } else if (window.location.pathname.substring(1, window.location.pathname.lastIndexOf('/')) === 'wines') {
 
         if (window.location.pathname.indexOf('-') === -1) {
             document.getElementById('pageSelected').innerHTML +=
-                '<div class="bg-black color-white text-vertical width-100% text-uppercase">' +
+                '<div class="bg-black color-white text-vertical width-100% text-uppercase font-secondary">' +
                 window.location.pathname.substring(7, window.location.pathname.lastIndexOf('.html')) + '</div>'
         } else if (window.location.pathname.substring(7, window.location.pathname.indexOf('vd'))) {
             document.getElementById('pageSelected').innerHTML +=
-                '<div class="bg-black color-white text-vertical width-100% text-uppercase">' +
+                '<div class="bg-black color-white text-vertical width-100% text-uppercase font-secondary">' +
                 window.location.pathname.substring(7, window.location.pathname.indexOf('-')) + '</div>'
 
         } else {
             document.getElementById('pageSelected').innerHTML +=
-                '<div class="bg-black color-white text-vertical width-100% text-uppercase">vineyard</div>'
+                '<div class="bg-black color-white text-vertical width-100% text-uppercase font-secondary">vineyard</div>'
 
         }
 
     } else {
         document.getElementById('pageSelected').innerHTML +=
-            '<div class="bg-black color-white text-vertical width-100% text-uppercase">' +
+            '<div class="bg-black color-white text-vertical width-100% text-uppercase font-secondary">' +
             window.location.pathname.substring(1, window.location.pathname.lastIndexOf('.html')) + '</div>'
     }
 
