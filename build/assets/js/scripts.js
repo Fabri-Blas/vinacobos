87ba6a510754c93cbc353b62e84eb2bac4ccb55c
bitbucket-pipeline <commits-noreply@bitbucket.com>
2022-05-03 12:52:58 +0000

    Build Number 323

--- build/assets/js/scripts.js
+++ build/assets/js/scripts.js
@@ -2620,6 +2620,32 @@ function headerLinksAnimation(links, opacity, transform, timing) {
         }, t += timing)
     })
 }
+const spanish = document.getElementById('es');
+const english = document.getElementById('en');
+
+spanish.addEventListener('click', function () {
+    if (spanish.classList.contains('color-language-active')) {
+        english.classList.remove('color-language-active');
+    } else {
+        spanish.classList.add('color-language-active');
+        english.classList.remove('color-language-active');
+    }
+    spanish.classList.add('color-language-active');
+    english.classList.remove('color-language-active');
+})
+
+english.addEventListener('click', function () {
+    if (english.classList.contains('color-language-active')) {
+        spanish.classList.remove('color-language-active');
+    } else {
+        english.classList.add('color-language-active');
+        spanish.classList.remove('color-language-active');
+    }
+    english.classList.add('color-language-active');
+    spanish.classList.remove('color-language-active');
+})
+
+
 const home = document.getElementById('home');
 const historia = document.getElementById('history');
 const identity = document.getElementById('identity');
